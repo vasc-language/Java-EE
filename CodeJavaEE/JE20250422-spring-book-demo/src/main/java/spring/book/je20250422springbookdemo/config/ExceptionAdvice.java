91c20d892a20317c9bc3c43531fddfa24c151a8a
vasc-language <2072855200@qq.com>
2025-04-25 21:48:52 +0800

    spring-book-demo

---
+++ CodeJavaEE/JE20250422-spring-book-demo/src/main/java/spring/book/je20250422springbookdemo/config/ExceptionAdvice.java
@@ -0,0 +1,12 @@
+package spring.book.je20250422springbookdemo.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 将异常统一处理，返回同一种形式
+ * User: 姚东名
+ * Date: 2025-04-25
+ * Time: 21:45
+ */
+
+public class ExceptionAdvice {
+}
