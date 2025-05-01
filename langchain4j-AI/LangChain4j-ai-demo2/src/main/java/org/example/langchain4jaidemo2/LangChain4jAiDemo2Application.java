776d5ddc94a05ea29d76b02e2a33a28abc58a0ea
vasc-language <2072855200@qq.com>
2025-05-01 10:19:44 +0800

    vasc-language

---
+++ langchain4j-AI/LangChain4j-ai-demo2/src/main/java/org/example/langchain4jaidemo2/LangChain4jAiDemo2Application.java
@@ -0,0 +1,13 @@
+package org.example.langchain4jaidemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangChain4jAiDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangChain4jAiDemo2Application.class, args);
+    }
+
+}
---
+++ langchain4j-AI/LangChain4j-ai-demo2/src/test/java/org/example/langchain4jaidemo2/LangChain4jAiDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.langchain4jaidemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangChain4jAiDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
