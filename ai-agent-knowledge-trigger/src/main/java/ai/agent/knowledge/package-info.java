a85969a188b04b925072073d5e543028945b1357
vasc-language <2072855200@qq.com>
2025-05-18 09:32:17 +0800

    1. init project

---
+++ ai-agent-knowledge-trigger/src/main/java/ai/agent/knowledge/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 姚东名
+ * Date: 2025-05-18
+ * Time: 9:20
+ */
+package ai.agent.knowledge;
---
+++ src/main/java/ai/agent/knowledge/Main.java
@@ -0,0 +1,14 @@
+package ai.agent.knowledge;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 姚东名
+ * Date: 2025-05-18
+ * Time: 8:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
